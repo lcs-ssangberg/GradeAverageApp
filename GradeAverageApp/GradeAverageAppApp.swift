158e8da4687625e75af824881ff6b38b735e2af4
Sebastian Sangberg <ssangberg@lcs.on.ca>
2025-02-27 09:43:12 -0500

    Initial Commit

---
+++ GradeAverageApp/GradeAverageAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradeAverageAppApp.swift
+//  GradeAverageApp
+//
+//  Created by Sebastian on 2025-02-27.
+//
+
+import SwiftUI
+
+@main
+struct GradeAverageAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
