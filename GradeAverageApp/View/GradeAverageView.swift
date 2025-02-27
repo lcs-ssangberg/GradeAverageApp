4e3f96de94e6513679e892a4b25fb477a1e7e6de
Sebastian Sangberg <ssangberg@lcs.on.ca>
2025-02-27 10:00:48 -0500

    got the app working, without the result history. The grade average and letter grade show with the three input fields also there

--- GradeAverageApp/View/GradeAverageView.swift
+++ GradeAverageApp/View/GradeAverageView.swift
@@ -8,12 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //MARK: stored properties
+    //tracks current state of data within the app
+    @State var viewModel = GradeAverageViewModel()
+    
+    //MARK: computed properties
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            //spacing
+            Spacer()
+            
+            
+            //Output
+            //check if the user returns correct input
+            if let  gradeAverage = viewModel.gradeAverage {
+                
+                VStack(alignment: .center) {
+                    
+                    //show average grade
+                    Text("Your Average Grade is: \(gradeAverage.average.formatted())")
+                        .font(.system(size: 100))
+                    
+                    //show letter grade
+                    Text("Your Letter Grade is: \(gradeAverage.letterGrade)")
+                        .font(.system(size: 100))
+                }
+                
+            } else {
+                
+                // Show a message indicating that we are
+                // awaiting reasonable input
+                ContentUnavailableView(
+                    "Unable to evaluate Average",
+                    systemImage: "gear.badge.questionmark",
+                    description: Text(viewModel.errorMessage)
+                )
+                .frame(height: 200)
+                
+            }
+            
+            // INPUT
+            TextField("Grade 1", text: $viewModel.providedGrade1)
+                .textFieldStyle(.roundedBorder)
+            
+            TextField("Grade 2", text: $viewModel.providedGrade2)
+                .textFieldStyle(.roundedBorder)
+            
+            TextField("Grade 3", text: $viewModel.providedGrade3)
+                .textFieldStyle(.roundedBorder)
+            
+            //spacing
+            Spacer()
         }
         .padding()
     }
